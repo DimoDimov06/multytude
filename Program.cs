a459f6003d7960b5c1a96abe20d92d84af093faa
PotatoW6 <anonymoushex6@gmail.com>
2024-04-24 08:49:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+       var m1 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+       var m2 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        var m3 = m1.Union(m2);
+        var m4 = m1.Intersect(m2);
+        var m5 = m1.Except(m2);
+        var m6 = m1.Concat(m2);
+
+        Console.WriteLine("Union: {0}", String.Join(" ", m3));
+        Console.WriteLine("Intersect: {0}", String.Join(" ", m4));
+        Console.WriteLine("Except: {0}", String.Join(" ", m5));
+        Console.WriteLine("Concat: {0}", String.Join(" ", m6));
+    }
+}
